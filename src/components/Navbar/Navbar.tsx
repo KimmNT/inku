3150b79faee060c22fee4b49d7ef7b6ab71f0072
KimmNT <kimnhatthien140220@gmail.com>
2025-09-11 16:58:55 +0700

    adjust navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+import { Link } from "@tanstack/react-router";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <Link to="/">Index</Link>
+      <Link to="/manga_search">Search</Link>
+    </div>
+  );
 }
--- src/views/Index/Index.tsx
+++ src/views/Index/Index.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Index() {
-  return <div>Index</div>;
+  return <div>Welcome to Inku - a reading place</div>;
 }
